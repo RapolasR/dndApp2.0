d557a5d69be973ef44a8d599671349599ee84ac1
RapolasR <rimkusrapolas@gmail.com>
2026-02-16 22:59:32 +0200

    sukuriau click check. pradejau main screen

--- dndApp2.0.py
+++ dndApp2.0.py
@@ -1,25 +1,26 @@
 ﻿
 import pygame
 
-# pygame setup
+# *** pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
 
-#frames
-activeF = 0
-# 0 - test screen
+# ***frames
+activeF = 1
+# *0 - test screen
+# *1 - main screen
 
 
 
-
+# *** Klase
 class Enemy:
     def __init__(self, name):
         self.name = name
 
-
+# *** funkcijos
 def drawButton(text, x, y, w, h, color = (0,0,0), fSize = 40):
     buttonRect = pygame.Rect(x, y, w, h)
     pygame.draw.rect(screen, color, buttonRect)
@@ -31,25 +32,50 @@ def drawButton(text, x, y, w, h, color = (0,0,0), fSize = 40):
     screen.blit(txt, txt_rect)
     return buttonRect
 
+
+#stuff
+enemies = []
+
+
+
+
 while running:
 
 
 
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    
 
     # uzpildyt ekrana
     screen.fill("purple")
 
-    # RENDER YOUR GAME HERE
+    # render cia
+    # --------------------------------frame'ai--------------------------------------
+
+    # --------------------------------0---------------------------------------------
     
-    if activeF == 0:
+    if activeF == 0:  
         testButton = drawButton("test", 50, 50, 500, 250)
 
+    # --------------------------------1---------------------------------------------
+
+    if activeF == 1: 
+        if enemies == []:
+            print(67)
 
 
+
+
+
+
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            if activeF == 0: #jei esam test frame'e
+                if testButton.collidepoint(e.pos):
+                    print('67 test')
 
 
 
