b8fb6ca6bb2fa39a485b4c814ac6cb8550b2cbf5
RapolasR <rimkusrapolas@gmail.com>
2026-02-17 09:16:50 +0200

    +Roll system, Universal buttons

--- dndApp2.0.py
+++ dndApp2.0.py
@@ -1,5 +1,7 @@
 ﻿
 import pygame
+import random as r
+
 
 # *** pygame setup
 pygame.init()
@@ -12,6 +14,7 @@ running = True
 activeF = 1
 # *0 - test screen
 # *1 - main screen
+# *2 - roll screen
 
 
 
@@ -33,16 +36,19 @@ def drawButton(text, x, y, w, h, color = (0,0,0), fSize = 40):
     return buttonRect
 
 
-#stuff
-enemies = []
+#variables n' stuff
+enemies = []    
+dice = [('d20', 20), ('d12', 12), ('d10', 10), ('d8', 8), ('d6', 6), ('d4', 4)]
 
 
+baseXdice = 225
+addXdice = 150
 
 
 while running:
 
 
-
+    rDice = []   #rendered Dice
 
     
 
@@ -52,30 +58,57 @@ while running:
     # render cia
     # --------------------------------frame'ai--------------------------------------
 
+    # ------------------------------universal---------------------------------------
+    backButton = drawButton('BACK', 1030, 0, 250, 50, (158, 2, 9))
+    
+    rollButton = drawButton('Roll', 830, 0, 200, 50)
+
     # --------------------------------0---------------------------------------------
     
     if activeF == 0:  
         testButton = drawButton("test", 50, 50, 500, 250)
 
-    # --------------------------------1---------------------------------------------
+    # --------------------------------2---------------------------------------------
 
-    if activeF == 1: 
-        if enemies == []:
-            print(67)
+    if activeF == 2:
+        
+        for index, die in enumerate(dice):
+            dName, value = die
+            addx = addXdice * index
+
+            dButton = drawButton(dName, baseXdice + addx, 200, 100, 100)
+            rDice.append((dButton, value))
+
+
+        
 
 
 
 
 
 
+
+    #event handling
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
         elif e.type == pygame.MOUSEBUTTONDOWN:
+
+            if backButton.collidepoint(e.pos):
+                activeF = 1
+            elif rollButton.collidepoint(e.pos):
+                activeF = 2
+
+
             if activeF == 0: #jei esam test frame'e
                 if testButton.collidepoint(e.pos):
                     print('67 test')
+            elif activeF == 2: 
+                for button in rDice:
+                    Button, value = button
+                    if Button.collidepoint(e.pos):
+                        print(r.randint(0, value))
 
 
 
